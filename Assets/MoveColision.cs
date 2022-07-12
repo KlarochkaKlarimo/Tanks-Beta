33bfa8e86eb8a9a8f8bbf371a57805fa536400a9
cubmasterr <62886240+cubmasterr@users.noreply.github.com>
2022-07-12 20:03:42 +0300

    fix

--- Assets/MoveColision.cs
+++ Assets/MoveColision.cs
@@ -1,52 +1,42 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class MoveColision : MonoBehaviour
 {
     [SerializeField] private PlayerMovement _playerMovement;
-    [SerializeField] private Collider collider ;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    [SerializeField] private Collider colliderLeft ;
+    [SerializeField] private Collider colliderRight ;
+    private bool left = false;
+    private bool right = false;
     private void OnCollisionStay(Collision collision)
     {
-        Collider myCollider = collision.GetContact(0).thisCollider;
+        var d = collision.contacts;
+        foreach (ContactPoint contact in d)
+        {
+            if (contact.thisCollider == colliderLeft)
+            {
+                left=true;
+            }
+            if (contact.thisCollider == colliderRight)
+            {
+                right = true;
+            }
+        }
         var isGround = collision.gameObject.layer == LayerMask.NameToLayer("ground");
-        if (isGround&& myCollider== collider)
+        if (isGround && left&& right)
         {
             print("hello");
             _playerMovement.setIsOnGround(true);
         }
     }
-    private void OnCollisionEnter(Collision collision)
-    {
-        Collider myCollider = collision.GetContact(0).thisCollider;
-        var isGround = collision.gameObject.layer == LayerMask.NameToLayer("ground");
-        if (isGround && myCollider == collider)
-        {
-            print("hello");
-            _playerMovement.setIsOnGround(true);
-        }
-    }
-
     private void OnCollisionExit(Collision collision)
     {
         var isGround = collision.gameObject.layer == LayerMask.NameToLayer("ground");
         if (isGround )
         {
-            print("bue");
+            left = false;
+             right = false;
+                print("bue");
             _playerMovement.setIsOnGround(false);
-
-
         }
     }
 }
