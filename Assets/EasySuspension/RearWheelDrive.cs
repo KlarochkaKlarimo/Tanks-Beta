72bace7953d118a91d2bca1ef8d9279e8721d56c
cubmasterr <62886240+cubmasterr@users.noreply.github.com>
2022-07-19 20:26:29 +0300

    fix

--- Assets/EasySuspension/RearWheelDrive.cs
+++ Assets/EasySuspension/RearWheelDrive.cs
@@ -3,30 +3,27 @@ using System.Collections;
 
 public class RearWheelDrive : MonoBehaviour {
 
-	private WheelCollider[] wheels;
+	private WheelCollider[] wheelsColliders;
+	public Transform[] wheels;
 
 	public float maxAngle = 30;
 	public float maxTorque = 300;
-	public GameObject wheelShape;
+	public Transform wheelShape;
 
 	// here we find all the WheelColliders down in the hierarchy
 	public void Start()
 	{
-		wheels = GetComponentsInChildren<WheelCollider>();
+		wheelsColliders = GetComponentsInChildren<WheelCollider>();
 
-		for (int i = 0; i < wheels.Length; ++ i) 
+		for (int i = 0; i < wheelsColliders.Length; ++i) 
 		{
-			var wheel = wheels [i];
+			var wheel = wheelsColliders [i];
 
 			// create wheel shapes only when needed
 			if (wheelShape != null)
 			{
-				var ws = Instantiate (wheelShape, wheel.transform);
-				if (i % 2 == 0)
-				{
-					ws.transform.Rotate(new Vector3(0,180,0));
-				}
-				//ws.transform.parent = wheel.transform;
+				var ws = GameObject.Instantiate (wheelShape);
+				ws.transform.parent = wheel.transform;
 			}
 		}
 	}
@@ -39,11 +36,11 @@ public class RearWheelDrive : MonoBehaviour {
 		float angle = maxAngle * Input.GetAxis("Horizontal");
 		float torque = maxTorque * Input.GetAxis("Vertical");
 
-		foreach (WheelCollider wheel in wheels)
+		foreach (WheelCollider wheel in wheelsColliders)
 		{
 			// a simple car where front wheels steer while rear ones drive
-			//if (wheel.transform.localPosition.z > 0)
-			//	wheel.steerAngle = angle;
+			if (wheel.transform.localPosition.z > 0)
+				wheel.steerAngle = angle;
 
 			if (wheel.transform.localPosition.z < 0)
 				wheel.motorTorque = torque;
@@ -54,14 +51,18 @@ public class RearWheelDrive : MonoBehaviour {
 				Quaternion q;
 				Vector3 p;
 				wheel.GetWorldPose (out p, out q);
-
+							
 				// assume that the only child of the wheelcollider is the wheel shape
-				Transform shapeTransform = wheel.transform.GetChild(0);
+				Transform shapeTransform = wheel.transform.GetChild (0);
 				shapeTransform.position = p;
-				shapeTransform.rotation = q;
+				shapeTransform.rotation =new Quaternion(shapeTransform.rotation.x,q.y,q.z,q.w) ;
 				shapeTransform.localScale = new Vector3(1, 1, 1);
 			}
 
+		}
+		foreach (Transform wheel in wheels)
+		{
+			wheel.Rotate(new Vector3(torque,0,0));
 		}
 	}
 }
