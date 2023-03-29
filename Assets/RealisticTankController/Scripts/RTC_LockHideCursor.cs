d493dcdcd4f6ffcdd0ed022f8a63748fc23a3a00
VasyanTugIr <92226196+VasyanTugIr@users.noreply.github.com>
2023-03-29 19:03:57 +0200

    commit fix legacy

---
+++ Assets/RealisticTankController/Scripts/RTC_LockHideCursor.cs
@@ -0,0 +1,26 @@
+﻿//----------------------------------------------
+//            Realistic Tank Controller
+//
+// Copyright © 2014 - 2017 BoneCracker Games
+// http://www.bonecrackergames.com
+// Buğra Özdoğanlar
+//
+//----------------------------------------------
+
+using UnityEngine;
+using System.Collections;
+
+[AddComponentMenu("BoneCracker Games/Realistic Tank Controller/UI/Lock Cursor")]
+public class RTC_LockHideCursor : MonoBehaviour {
+
+
+	void Awake (){
+		
+		Cursor.lockState = CursorLockMode.Locked;
+		Cursor.visible = false;
+
+	}
+	
+	
+
+}
