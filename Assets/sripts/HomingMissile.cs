f2f702d44424b932fe76c9e04de95e74f25a53fe
VasyanTugIr <92226196+VasyanTugIr@users.noreply.github.com>
2023-10-23 21:21:23 +0300

    dobavil protype raketi spike. Vrode rabotatet, no nujno dodelat

---
+++ Assets/sripts/HomingMissile.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HomingMissile : MonoBehaviour
+{
+    public float speed = 10f;
+    [SerializeField] private Transform target;
+    [SerializeField] private TargetLock targetLock;
+    [SerializeField] private GameObject _flightFire;
+    [SerializeField] private GameObject _flightFire1;
+
+    private void Update()
+    {
+        targetLock = FindObjectOfType<TargetLock>();
+
+        if (targetLock != null)
+        {
+            target = targetLock.GetLockedTarget();
+        }
+
+        if (target != null)
+        {
+            transform.SetParent(null);
+
+            _flightFire.SetActive(true);
+            _flightFire1.SetActive(true);
+
+            transform.rotation = Quaternion.Euler(90f, 0f, 0f);
+            // Направление движения ракеты к цели
+            Vector3 targetDirection = target.position - transform.position;
+
+            // Поворот ракеты к цели
+            transform.rotation = Quaternion.LookRotation(targetDirection);
+
+            // Движение ракеты в направлении цели
+            transform.Translate(Vector3.forward * speed * Time.deltaTime);
+        }
+    }
+
+    
+}
---
+++ Assets/sripts/TargetLock.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TargetLock : MonoBehaviour
+{
+    public float maxDistance;
+
+    [SerializeField] private GameObject targetCaptured;
+    [SerializeField] private GameObject targetNotCaptured;
+    [SerializeField] private Transform _lockedTarget;
+    [SerializeField] private bool isLocked = false;
+    [SerializeField] private HomingMissile missileScript; // Ссылка на скрипт ракеты
+
+    private void Start()
+    {      
+       // Найдите скрипт ракеты на текущем объекте
+        missileScript = GetComponent<HomingMissile>();
+    }
+
+    private void Update()
+    {
+        if (isLocked)
+        {
+            // Здесь вы можете добавить логику для удержания цели и отслеживания ее движения.
+        }
+        else
+        {
+            // Если цель не захвачена, проверьте, смотрите ли вы на цель и нажимаете ли кнопку для захвата (например, X).
+            if (Input.GetKeyDown(KeyCode.X))
+            {
+                RaycastHit hit;
+                if (Physics.Raycast(transform.position, transform.forward, out hit, maxDistance))
+                {
+                    if (hit.collider.CompareTag("Tank"))
+                    {
+                        _lockedTarget = hit.collider.transform;
+                        isLocked = true;       
+                        if (isLocked)
+                        {
+                            targetNotCaptured.SetActive(false);
+                            targetCaptured.SetActive(true);
+                        }
+
+
+
+                        //HomingMissile _homingMissile = FindObjectOfType<HomingMissile>();
+                        //if (_homingMissile != null)
+                        //{
+                        //    _homingMissile.GetTarget(_lockedTarget);
+                        //}
+
+                        
+                    }
+                }
+            }
+        }
+    }
+
+    public Transform GetLockedTarget()
+    {
+        return _lockedTarget;
+    }
+}
