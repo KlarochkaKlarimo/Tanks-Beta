a2a9dc1b81329feec857c10da24eacd001928a4c
VasyanTugIr <92226196+VasyanTugIr@users.noreply.github.com>
2023-12-24 20:23:49 +0300

    SmokeGrenadesAdd

---
+++ Assets/sripts/SmokeGrenadesScrip.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SmokeGrenadesScrip : MonoBehaviour
+{
+    public float launchForce = 10f;
+    [SerializeField] private float _explosionTime;
+    [SerializeField] private GameObject _SmokeGrenade;
+    [SerializeField] private GameObject _explosionEffect;
+
+    public void LaunchSmokeGrenade()
+    {
+        Rigidbody rb = GetComponent<Rigidbody>();
+        if (rb != null)
+        {
+            rb.isKinematic = false;
+            Vector3 launchDirection = _SmokeGrenade.transform.forward;
+
+            rb.AddForce(launchDirection * launchForce, ForceMode.Impulse);
+            Invoke("Explosion", _explosionTime);
+        }
+    }
+    private void Explosion()
+    {
+        Instantiate(_explosionEffect, transform.position, Quaternion.identity);
+        Destroy(gameObject);
+    }
+}
