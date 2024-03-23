329ee44d9e30fb06266f85d3e7e1ad93dab04dbf
VasyanTugIr <92226196+VasyanTugIr@users.noreply.github.com>
2024-03-23 21:40:21 +0300

    Mine Trawl

--- Assets/sripts/MineScript.cs
+++ Assets/sripts/MineScript.cs
@@ -24,7 +24,6 @@ public class MineScript : MonoBehaviour
             _hasCollided = true;
             StartCoroutine(ExplodeAfterDelay());            
         }
-
     }
     
     private IEnumerator ExplodeAfterDelay()
--- Assets/sripts/minniyTral.cs
+++ Assets/sripts/minniyTral.cs
@@ -4,18 +4,20 @@ using UnityEngine;
 
 public class minniyTral : MonoBehaviour
 {   
-    [SerializeField] private Vector3 _rotation;
+    private Vector3 _rotation;
+    [SerializeField] private float _minValue;
+    [SerializeField] private float _maxValue;
     [SerializeField] private float _speed;
 
     void Update()
-    {
-        if (Input.GetKey(KeyCode.PageDown))
+    {       
+        if (Input.GetKey(KeyCode.PageDown) && transform.localEulerAngles.x < _maxValue)
         {
             _rotation = Vector3.right;
             transform.Rotate(_rotation * _speed * Time.deltaTime);
         }
 
-        else if (Input.GetKey(KeyCode.PageUp))
+        else if (Input.GetKey(KeyCode.PageUp) && transform.localEulerAngles.x > _minValue)
         {
             _rotation = Vector3.left;
             transform.Rotate(_rotation * _speed * Time.deltaTime);
